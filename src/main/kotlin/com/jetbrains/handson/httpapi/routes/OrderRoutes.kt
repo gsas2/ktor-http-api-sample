5a4470c7efea74af05aa9a6ad546423e97c9ca38
Gastón Sas <gsas@asterix.local>
2022-02-20 19:42:40 -0300

    Completing tutorial (with Customers and Orders endpoints)

---
+++ src/main/kotlin/com/jetbrains/handson/httpapi/routes/OrderRoutes.kt
@@ -0,0 +1,56 @@
+package com.jetbrains.handson.httpapi.routes
+
+import com.jetbrains.handson.httpapi.models.*
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
+
+fun Route.listOrdersRoute() {
+    get("/order") {
+        if (orderStorage.isNotEmpty()) {
+            call.respond(orderStorage)
+        }
+    }
+}
+
+fun Route.getOrderRoute() {
+    get("/order/{id}") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        call.respond(order)
+    }
+}
+
+fun Route.addOrderRoute() {
+    post("/order") {
+        val order = call.receive<Order>()
+        orderStorage.add(order)
+        call.respondText("Order stored correctly", status = HttpStatusCode.Created)
+    }
+}
+
+fun Route.totalizeOrderRoute() {
+    get("/order/{id}/total") {
+        val id = call.parameters["id"] ?: return@get call.respondText("Bad Request", status = HttpStatusCode.BadRequest)
+        val order = orderStorage.find { it.number == id } ?: return@get call.respondText(
+            "Not Found",
+            status = HttpStatusCode.NotFound
+        )
+        val total = order.contents.sumOf { it.price * it.amount }
+        call.respond(total)
+    }
+}
+
+fun Application.registerOrderRoutes() {
+    routing {
+        listOrdersRoute()
+        getOrderRoute()
+        addOrderRoute()
+        totalizeOrderRoute()
+    }
+}
